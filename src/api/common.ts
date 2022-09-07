3cca91e79a36cf5ee32111453e410f1abfd53117
wiredmartian <solomzi.jikani@gmail.com>
2022-09-07 23:05:39 +0200

    actions update

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,8 +1,8 @@
 import axios, {AxiosRequestConfig} from "axios"
 
-axios.defaults.baseURL = "http://localhost:9200/api/v1"
+// axios.defaults.baseURL = "http://localhost:9200/api/v1"
 axios.defaults.timeout = 10000
-// axios.defaults.baseURL = "https://menu.core.wiredmartians.co.za/api/v1"
+axios.defaults.baseURL = "https://menu.core.wiredmartians.co.za/api/v1"
 
 
 axios.interceptors.request.use(
@@ -29,5 +29,5 @@ axios.interceptors.request.use(
 
 export const $axios = axios
 
-export const IMGCDN = "http://localhost:9200"
+export const IMGCDN = "https://menu.core.wiredmartians.co.za"
 
