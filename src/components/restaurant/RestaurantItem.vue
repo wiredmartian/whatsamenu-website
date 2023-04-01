6c4b8494fe7ecec76d683be4e8714f65af3adf8a
wiredmartian <solomzi.jikani@gmail.com>
2023-04-01 22:52:57 +0200

    updated padding

--- src/components/restaurant/RestaurantItem.vue
+++ src/components/restaurant/RestaurantItem.vue
@@ -3,7 +3,8 @@
     <div class="media mb-4" :key="restaurant.name">
       <div class="row">
         <div class="col-md-12">
-          <img v-if="restaurant.imageUrl" class="img-restaurant rounded mr-3 w-100" :src="`${imgCDN}/${restaurant.imageUrl}`"
+          <img v-if="restaurant.imageUrl" class="img-restaurant rounded mr-3 w-100"
+               :src="`${imgCDN}/${restaurant.imageUrl}`"
                :alt="restaurant.name">
         </div>
         <div class="col-md-12">
@@ -16,7 +17,9 @@
               {{ restaurant.address.city }}, {{ restaurant.address.state }}
             </p>
             <p class="lead">
-              <span class="badge badge-dark rounded-pill">{{ formatDistance(restaurant.distance) }}</span>
+              <span class="badge badge-dark rounded-pill p-2">{{
+                  formatDistance(restaurant.distance)
+                }}</span>
             </p>
           </div>
         </div>
@@ -28,7 +31,7 @@
 <script lang="ts">
 import Vue from "vue"
 import {Restaurant} from "@/types/types";
-import { IMGCDN } from "@/api/common";
+import {IMGCDN} from "@/api/common";
 
 export default Vue.extend({
   name: "RestaurantItem",
