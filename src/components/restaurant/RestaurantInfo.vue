bd6161116e72402a3acf43a5cd748c9e382d9153
wiredmartian <solomzi.jikani@gmail.com>
2023-05-27 12:26:53 +0200

    modal for more restaurant info

--- src/components/restaurant/RestaurantInfo.vue
+++ src/components/restaurant/RestaurantInfo.vue
@@ -6,20 +6,66 @@
     <p v-if="restaurant.address" class="text-muted pl-4">{{ restaurant.address.line1 }}, {{
         restaurant.address.line2
       }} <br/>
-      {{ restaurant.address.city }}, {{ provinceMap[restaurant.address.state] }}
+      {{ restaurant.address.city }}, {{ provinceMap[restaurant.address.state] }} &nbsp; 
+      <span class="badge badge-light rounded-pill" data-toggle="modal" data-target="#restaurantDetail" role="button">More info</span>
     </p>
+
+      <!-- Modal -->
+  <div  :key="restaurant.restaurantId" class="modal fade"
+      id="restaurantDetail" tabindex="-1" role="dialog" aria-labelledby="restaurantDetailTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-md" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" :id="restaurant.restaurantId">{{ restaurant.name }}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+                <div class="w-100">
+                  <div class="col">
+                    <img v-if="restaurant.imageUrl"
+                        v-bind:src="`${cdn}/${restaurant.imageUrl}`"
+                        class="w-100 d-block m-auto"
+                        :alt="restaurant.name">
+                  </div>
+                  <div class="col mt-2 mb-4">
+                    <div class="description-content">
+                      <h2 class="font-weight-normal pb-2">
+                        {{ restaurant.name }}
+                      </h2>
+                      <p class="text-muted">{{ restaurant.summary }}</p>
+                      <hr/>
+                      <location-map v-if="restaurant && restaurant.address" :longitude="restaurant.address.longitude"
+            :latitude="restaurant.address.latitude" />
+                    </div>
+                    <hr/>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Modal -->
   </div>
 </template>
 
 <script lang="ts">
-import {ProvincesMap, Restaurant} from "@/types"
 import Vue from "vue"
+import {ProvincesMap, Restaurant} from "@/types"
+import {IMGCDN} from "@/api/common";
 
 export default Vue.extend({
   name: "RestaurantInfo",
+  components: {
+    "location-map": () => import("@/components/restaurant/LocationMap.vue"),
+  },
   data() {
     return {
-      provinceMap: ProvincesMap
+      provinceMap: ProvincesMap,
+      cdn: IMGCDN
     }
   },
   props: {
@@ -29,9 +75,6 @@ export default Vue.extend({
       required: true
     }
   },
-  mounted() {
-    console.log(this.restaurant)
-  }
 })
 </script>
 
