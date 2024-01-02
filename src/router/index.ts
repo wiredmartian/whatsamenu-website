a4458f41ac5149fd44f5903e7e999dc0fa8f8bf4
wiredmartian <solomzi.jikani@gmail.com>
2024-01-02 22:16:45 +0200

    remove gpt menu enquire implementation for now

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,48 +1,43 @@
-import Vue from 'vue'
-import VueRouter, {RouteConfig} from 'vue-router'
+import Vue from "vue"
+import VueRouter, { RouteConfig } from "vue-router"
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
     {
-        path: '/',
-        name: 'home',
+        path: "/",
+        name: "home",
         component: () => import("@/views/HomeView.vue")
     },
     {
-        path: '/get-started',
-        name: 'get-started',
+        path: "/get-started",
+        name: "get-started",
         component: () => import("@/views/account/GetStarted.vue")
     },
     {
-        path: '/login',
-        name: 'login',
+        path: "/login",
+        name: "login",
         component: () => import("@/views/account/UserLogin.vue")
     },
     {
-        path: '/account/api-keys',
-        name: 'api-keys',
+        path: "/account/api-keys",
+        name: "api-keys",
         component: () => import("@/views/account/CreateAPIKey.vue")
     },
     {
-        path: '/restaurants/:id/menu',
-        name: 'menu',
+        path: "/restaurants/:id/menu",
+        name: "menu",
         component: () => import("@/views/menu/MenuView.vue")
     },
     {
-        path: '/restaurant/menu/menu-item/:id',
-        name: 'menu-item',
+        path: "/restaurant/menu/menu-item/:id",
+        name: "menu-item",
         component: () => import("@/views/menu/MenuItemView.vue")
-    },
-    {
-        path: '/menu/:id/enquire',
-        name: 'menu enquiry',
-        component: () => import('@/views/gpt/MenuEnquiryView.vue')
     }
 ]
 
 const router = new VueRouter({
-    mode: 'history',
+    mode: "history",
     base: process.env.BASE_URL,
     routes
 })
